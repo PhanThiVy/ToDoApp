48c611755fd30aee2dac0d8422c1cc3990a0e2eb
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 10:20:06 +0700

    create TaskRequestDto in responseDto

---
+++ src/main/java/com/example/ToDoAppDemo/dto/responseDto/TaskResponseDto.java
@@ -0,0 +1,22 @@
+package com.example.ToDoAppDemo.dto.responseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class TaskResponseDto {
+    private long taskId;
+    private String taskName;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private LocalDate updateDate;
+    private String taskListName;
+}
