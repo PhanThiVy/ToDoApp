2ad96fcef3e8e814ebbe23c84e90e2c5915aca9f
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 10:05:17 +0700

    create User.java

---
+++ src/main/java/com/example/ToDoAppDemo/model/User.java
@@ -0,0 +1,72 @@
+package com.example.ToDoAppDemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 60)
+    private String fullName;
+    @Column(nullable = false, unique = true,length = 60)
+    private String userName;
+    @Column(nullable = false,length = 60)
+    private String password;
+    @Email
+    @Column(nullable = false,unique = true,length = 60)
+    private String email;
+    @Column(nullable = false)
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private LocalDate registrationDate = LocalDate.now();
+    //    @Builder.Default
+    private Boolean locked = false;
+
+    //    @Builder.Default
+    private Boolean enabled = false;
+    private String token;
+    @ManyToMany(cascade =
+            {
+                    CascadeType.DETACH,
+                    CascadeType.MERGE,
+                    CascadeType.REFRESH,
+                    CascadeType.PERSIST
+            }, fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private List<Role> roles = new ArrayList<>();
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<TaskList> taskList = new ArrayList<>();
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public void removeRole(Role role) {
+        roles.remove(role);
+    }
+    public void addTaskList(TaskList tasks) {
+        taskList.add(tasks);
+    }
+
+    public void removeTaskList(TaskList tasks) {
+        taskList.remove(tasks);
+    }
+}
