dffcd77794e5dce4bc2c6edf4c2b1f76f7114214
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 11:10:18 +0700

    declare method addUser,getUser,getUserById,addRoleToUserToUser,deleteRoleFromUser,userNameIsExist in UserService

--- src/main/java/com/example/ToDoAppDemo/service/iService/UserService.java
+++ src/main/java/com/example/ToDoAppDemo/service/iService/UserService.java
@@ -1,7 +1,18 @@
 package com.example.ToDoAppDemo.service.iService;
 
+import com.example.ToDoAppDemo.dto.requestDto.UserRequestDto;
+import com.example.ToDoAppDemo.dto.responseDto.UserResponseDto;
+import com.example.ToDoAppDemo.model.User;
 import org.springframework.stereotype.Service;
 
 
 public interface UserService {
+    public UserResponseDto addUser(UserRequestDto userRequestDto);
+    public User getUser(String userId);
+    public UserResponseDto getUserById(String userId);
+    public UserResponseDto addAdmin(UserRequestDto userRequestDto);
+    public UserResponseDto addRoleToUserToUser(String userId, String roleId);
+    public UserResponseDto deleteRoleFromUser(String bookId, String roleId);
+    public boolean isNumberic(String userId);
+    public boolean userNameIsExist(String userName);
 }
