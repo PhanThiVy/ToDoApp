f2283b7a35ce5bc6342e83bc1f39a725b81eafe9
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 12:01:00 +0700

    create method handlerUserNotValidException() in ApplicationExceptionHandler

--- src/main/java/com/example/ToDoAppDemo/exception/ApplicationExceptionHandler.java
+++ src/main/java/com/example/ToDoAppDemo/exception/ApplicationExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.ToDoAppDemo.exception;
 
 import com.example.ToDoAppDemo.exception.userException.UserNameExistException;
+import com.example.ToDoAppDemo.exception.userException.UserNotValidException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,6 +12,11 @@ public class ApplicationExceptionHandler {
     @ExceptionHandler(UserNameExistException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorDetail handlerUserNameExistException(UserNameExistException ex){
+        return ex.getErrorDetail();
+    }
+    @ExceptionHandler(UserNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDetail handlerUserNotValidException(UserNotValidException ex){
         return ex.getErrorDetail();
     }
 }
