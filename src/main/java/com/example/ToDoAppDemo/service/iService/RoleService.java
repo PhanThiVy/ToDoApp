dd9d3e31e5560d43656f07ef5f09391d968fc4c3
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 11:25:21 +0700

    declare method getRoleByName(String roleName) in RoleRepository.java

--- src/main/java/com/example/ToDoAppDemo/service/iService/RoleService.java
+++ src/main/java/com/example/ToDoAppDemo/service/iService/RoleService.java
@@ -1,8 +1,10 @@
 package com.example.ToDoAppDemo.service.iService;
 
 import com.example.ToDoAppDemo.dto.responseDto.RoleResponseDto;
+import com.example.ToDoAppDemo.model.Role;
 import org.springframework.data.domain.Page;
 
 public interface RoleService {
     public Page<RoleResponseDto> getRoleList(int pageNumber);
+    public Role getRoleByName(String roleName);
 }
