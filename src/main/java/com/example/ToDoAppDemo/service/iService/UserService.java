d7ee07152c78ab9250deb4d09657921c96956eeb
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 14:04:20 +0700

    declare method addTaskList(User user, TaskList taskList) and removeTaskList(User user, TaskList taskList) in UserService.java

--- src/main/java/com/example/ToDoAppDemo/service/iService/UserService.java
+++ src/main/java/com/example/ToDoAppDemo/service/iService/UserService.java
@@ -2,6 +2,7 @@ package com.example.ToDoAppDemo.service.iService;
 
 import com.example.ToDoAppDemo.dto.requestDto.UserRequestDto;
 import com.example.ToDoAppDemo.dto.responseDto.UserResponseDto;
+import com.example.ToDoAppDemo.model.TaskList;
 import com.example.ToDoAppDemo.model.User;
 import org.springframework.stereotype.Service;
 
@@ -13,4 +14,6 @@ public interface UserService {
     public UserResponseDto addRoleToUserToUser(String userId, String roleId);
     public UserResponseDto deleteRoleFromUser(String bookId, String roleId);
     public boolean userNameIsExist(String userName);
+    void addTaskList(User user, TaskList taskList);
+    void removeTaskList(User user, TaskList taskList);
 }
