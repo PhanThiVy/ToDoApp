e0fe01b75f86bc92013f99893f0fe33da0742448
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 10:08:33 +0700

    create interface RoleRepository

---
+++ src/main/java/com/example/ToDoAppDemo/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.ToDoAppDemo.repository;
+
+import com.example.ToDoAppDemo.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Long> {
+}
