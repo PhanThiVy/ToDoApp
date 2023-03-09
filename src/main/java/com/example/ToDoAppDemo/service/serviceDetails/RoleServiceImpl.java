e2c1095912505d9f624c5dff639f0d78adaedd89
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 11:01:17 +0700

    overide method getRoleList in RoleServiceImpl

--- src/main/java/com/example/ToDoAppDemo/service/serviceDetails/RoleServiceImpl.java
+++ src/main/java/com/example/ToDoAppDemo/service/serviceDetails/RoleServiceImpl.java
@@ -1,8 +1,24 @@
 package com.example.ToDoAppDemo.service.serviceDetails;
 
+import com.example.ToDoAppDemo.dto.mapper.Mapper;
+import com.example.ToDoAppDemo.dto.responseDto.RoleResponseDto;
+import com.example.ToDoAppDemo.model.Role;
+import com.example.ToDoAppDemo.repository.RoleRepository;
 import com.example.ToDoAppDemo.service.iService.RoleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RoleServiceImpl implements RoleService {
+    private final RoleRepository roleRepository;
+    private final PageRequest rolePageable;
+    @Override
+    public Page<RoleResponseDto> getRoleList(int pageNumber) {
+        Page<Role> rolePage = roleRepository.findAll(rolePageable.withPage(pageNumber));
+        return rolePage.map(role -> Mapper.roleToRoleResponseDto(role));
+    }
 }
