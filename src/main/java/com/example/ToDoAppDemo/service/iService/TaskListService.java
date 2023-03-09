d491a8831881e700cbc5a0bcc5f94d31c9001998
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 13:37:18 +0700

    declare 2 method addTaskList(TaskListRequestDto taskListRequestDto) and roleNameIsExistForAdd(String taskListName, Long userId) in TaskListService.java

--- src/main/java/com/example/ToDoAppDemo/service/iService/TaskListService.java
+++ src/main/java/com/example/ToDoAppDemo/service/iService/TaskListService.java
@@ -1,7 +1,11 @@
 package com.example.ToDoAppDemo.service.iService;
 
+import com.example.ToDoAppDemo.dto.requestDto.TaskListRequestDto;
+import com.example.ToDoAppDemo.dto.responseDto.TaskListResponseDto;
+import com.example.ToDoAppDemo.model.TaskList;
 import org.springframework.stereotype.Service;
 
 public interface TaskListService {
-
+    public TaskListResponseDto addTaskList(TaskListRequestDto taskListRequestDto);
+    public void roleNameIsExistForAdd(String taskListName,Long userId);
 }
