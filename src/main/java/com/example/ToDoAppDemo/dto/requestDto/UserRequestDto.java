5f203e9a1f3df6f43b7b2fa21c270329f1a012c1
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 10:15:27 +0700

    create userRequestDto in requestDto

---
+++ src/main/java/com/example/ToDoAppDemo/dto/requestDto/UserRequestDto.java
@@ -0,0 +1,29 @@
+package com.example.ToDoAppDemo.dto.requestDto;
+
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+public class UserRequestDto {
+    @NotEmpty
+    @Size(min = 5,max = 60)
+    private String fullName;
+    @NotEmpty
+    @Size(min = 5,max = 60)
+    private String userName;
+
+    @NotEmpty
+    @Size(min = 3,max = 30)
+    private String password;
+
+    @NotEmpty
+    @Email
+    private String email;
+}
