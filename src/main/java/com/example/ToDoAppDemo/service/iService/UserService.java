4783fc0e299bb6b60867396f83cd97a3c2e1c9ea
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 12:02:24 +0700

    delete method addAdmin() in UserService

--- src/main/java/com/example/ToDoAppDemo/service/iService/UserService.java
+++ src/main/java/com/example/ToDoAppDemo/service/iService/UserService.java
@@ -10,9 +10,7 @@ public interface UserService {
     public UserResponseDto addUser(UserRequestDto userRequestDto);
     public User getUser(String userId);
     public UserResponseDto getUserById(String userId);
-    public UserResponseDto addAdmin(UserRequestDto userRequestDto);
     public UserResponseDto addRoleToUserToUser(String userId, String roleId);
     public UserResponseDto deleteRoleFromUser(String bookId, String roleId);
-    public boolean isNumberic(String userId);
     public boolean userNameIsExist(String userName);
 }
