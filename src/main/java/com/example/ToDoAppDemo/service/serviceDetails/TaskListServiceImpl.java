eb0b7563a077c0b526a92d54dc13b5aa773036ae
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 14:08:22 +0700

    overide method addTaskList(TaskListRequestDto taskListRequestDto) in TaskListServiceImpl.java

--- src/main/java/com/example/ToDoAppDemo/service/serviceDetails/TaskListServiceImpl.java
+++ src/main/java/com/example/ToDoAppDemo/service/serviceDetails/TaskListServiceImpl.java
@@ -1,11 +1,15 @@
 package com.example.ToDoAppDemo.service.serviceDetails;
 
+import com.example.ToDoAppDemo.dto.mapper.Mapper;
 import com.example.ToDoAppDemo.dto.requestDto.TaskListRequestDto;
 import com.example.ToDoAppDemo.dto.responseDto.TaskListResponseDto;
 import com.example.ToDoAppDemo.exception.taskListException.TaskListNameIsExisException;
 import com.example.ToDoAppDemo.model.TaskList;
+import com.example.ToDoAppDemo.model.User;
 import com.example.ToDoAppDemo.repository.TaskListRepository;
+import com.example.ToDoAppDemo.repository.UserRepository;
 import com.example.ToDoAppDemo.service.iService.TaskListService;
+import com.example.ToDoAppDemo.service.iService.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -14,9 +18,23 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class TaskListServiceImpl implements TaskListService {
     private final TaskListRepository taskListRepository;
+    private final UserService userService;
     @Override
     public TaskListResponseDto addTaskList(TaskListRequestDto taskListRequestDto) {
-        return null;
+        TaskList taskList = new TaskList();
+        //check user is exist
+        User user = userService.getUser(taskListRequestDto.getUserId());
+        //check task list name is exist
+        Long userId = Long.valueOf(taskListRequestDto.getUserId());
+        roleNameIsExistForAdd(taskListRequestDto.getTaskListName(),userId);
+
+        taskList.setListName(taskListRequestDto.getTaskListName());
+        taskList.setUser(user);
+        //save task list
+        taskListRepository.save(taskList);
+        //add task list to user
+        userService.addTaskList(user,taskList);
+        return Mapper.TaskListToTaskListResponseDto(taskList);
     }
 
     @Override
@@ -26,4 +44,5 @@ public class TaskListServiceImpl implements TaskListService {
             throw new TaskListNameIsExisException(HttpStatus.CONFLICT.value(), " This task list name is exist - please enter a new one");
         }
     }
+
 }
