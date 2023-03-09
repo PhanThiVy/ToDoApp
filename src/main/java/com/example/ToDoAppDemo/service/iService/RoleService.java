fd379f86490ee06b685a41a76057790666b1b9d0
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 11:00:41 +0700

    create method getRoleList() in RoleService

--- src/main/java/com/example/ToDoAppDemo/service/iService/RoleService.java
+++ src/main/java/com/example/ToDoAppDemo/service/iService/RoleService.java
@@ -1,4 +1,8 @@
 package com.example.ToDoAppDemo.service.iService;
 
+import com.example.ToDoAppDemo.dto.responseDto.RoleResponseDto;
+import org.springframework.data.domain.Page;
+
 public interface RoleService {
+    public Page<RoleResponseDto> getRoleList(int pageNumber);
 }
