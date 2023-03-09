9be6884e17e81a6b235f54ad00708d02e5c78c91
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 10:18:38 +0700

    create userRequestDto in responseDto

---
+++ src/main/java/com/example/ToDoAppDemo/dto/responseDto/UserResponseDto.java
@@ -0,0 +1,25 @@
+package com.example.ToDoAppDemo.dto.responseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class UserResponseDto {
+    private Long id;
+    private String fullName;
+    private String userName;
+    private String password;
+    private String email;
+    private List<String> roleNames;
+    private LocalDate registrationDate;
+    private Boolean locked;
+    private Boolean enabled;
+    private List<String> taskListName;
+}
