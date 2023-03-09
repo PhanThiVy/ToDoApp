688045e71ccb671baf84954e8836c339f634d360
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 12:05:59 +0700

    create API 'sinUp' in UserController.java

---
+++ src/main/java/com/example/ToDoAppDemo/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.example.ToDoAppDemo.controller;
+
+import com.example.ToDoAppDemo.dto.requestDto.UserRequestDto;
+import com.example.ToDoAppDemo.dto.responseDto.UserResponseDto;
+import com.example.ToDoAppDemo.exception.userException.UserNotValidException;
+import com.example.ToDoAppDemo.service.iService.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+    @PostMapping("/signUp")
+    public ResponseEntity<UserResponseDto> addUser(@Valid @RequestBody UserRequestDto userRequestDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+
+            bindingResult.getFieldErrors().forEach(
+                    error -> errors.put(error.getField(), error.getDefaultMessage())
+            );
+
+            String errorMsg = "";
+
+            for (String key : errors.keySet()) {
+                errorMsg += "Error by: " + key + ", reason: " + errors.get(key) + " - ";
+            }
+            throw new UserNotValidException(HttpStatus.BAD_REQUEST.value(), errorMsg);
+        }
+        UserResponseDto userResponseDto = userService.signUp(userRequestDto);
+        return new ResponseEntity<>(userResponseDto, HttpStatus.OK);
+    }
+
+}
