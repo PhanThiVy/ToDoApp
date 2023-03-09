970bb93239f9af87c3363c9a08309eeaa1f4c864
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 12:11:11 +0700

    create UserNotFoundException.java

---
+++ src/main/java/com/example/ToDoAppDemo/exception/userException/UserNotFoundException.java
@@ -0,0 +1,19 @@
+package com.example.ToDoAppDemo.exception.userException;
+
+import com.example.ToDoAppDemo.exception.ErrorDetail;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class UserNotFoundException extends RuntimeException{
+    private final ErrorDetail errorDetail;
+
+    public UserNotFoundException(int errorCode, String message) {
+        super();
+        this.errorDetail = new ErrorDetail().builder()
+                .errorCode(errorCode)
+                .message(message)
+                .build();
+    }
+}
