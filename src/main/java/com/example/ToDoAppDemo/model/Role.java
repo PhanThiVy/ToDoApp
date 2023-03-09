107687599442b63f71f0ffa6f8e2e2e2747c8145
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 10:04:49 +0700

    create Role.java

---
+++ src/main/java/com/example/ToDoAppDemo/model/Role.java
@@ -0,0 +1,40 @@
+package com.example.ToDoAppDemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true,length = 20)
+    private String roleName;
+    @ManyToMany(mappedBy = "roles",cascade =
+            {
+                    CascadeType.DETACH,
+                    CascadeType.MERGE,
+                    CascadeType.REFRESH,
+                    CascadeType.PERSIST
+            }, fetch = FetchType.LAZY)
+    private List<User> users = new ArrayList<>();
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public void removeUser(User user) {
+        users.remove(user);
+    }
+}
