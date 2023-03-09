9fd3b576efb3325198cc43d63f56243573f7991c
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 13:43:55 +0700

    create TaskListNameIsExisException

---
+++ src/main/java/com/example/ToDoAppDemo/exception/taskListException/TaskListNameIsExisException.java
@@ -0,0 +1,19 @@
+package com.example.ToDoAppDemo.exception.taskListException;
+
+import com.example.ToDoAppDemo.exception.ErrorDetail;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class TaskListNameIsExisException extends RuntimeException{
+    private final ErrorDetail errorDetail;
+
+    public TaskListNameIsExisException(int errorCode, String message) {
+        super();
+        this.errorDetail = new ErrorDetail().builder()
+                .errorCode(errorCode)
+                .message(message)
+                .build();
+    }
+}
