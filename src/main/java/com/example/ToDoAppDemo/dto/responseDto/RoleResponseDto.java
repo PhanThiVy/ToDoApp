0295c6e7884b686f731b31763c9dc43fa70f07ec
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 10:20:48 +0700

    create RoleRequestDto in responseDto

---
+++ src/main/java/com/example/ToDoAppDemo/dto/responseDto/RoleResponseDto.java
@@ -0,0 +1,18 @@
+package com.example.ToDoAppDemo.dto.responseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class RoleResponseDto {
+    private Long id;
+    private String roleName;
+    private List<String> userNames;
+}
