293bb00f63b69151a4e58cd1bfe24b04e290dc6e
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 10:05:39 +0700

    create TaskList.java

---
+++ src/main/java/com/example/ToDoAppDemo/model/TaskList.java
@@ -0,0 +1,29 @@
+package com.example.ToDoAppDemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "task_list")
+public class TaskList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long taskListId;
+    @Column(nullable = false, length = 20,unique = true)
+    private String listName;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, referencedColumnName = "id")
+    private User user;
+    @OneToMany(mappedBy = "taskList", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Task> tasks = new ArrayList<>();
+}
