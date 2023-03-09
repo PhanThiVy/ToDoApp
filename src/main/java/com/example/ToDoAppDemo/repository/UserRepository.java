98757407e22b697cc4ef0da13c114fda4b751a6a
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 10:09:22 +0700

    create interface UserRepository

---
+++ src/main/java/com/example/ToDoAppDemo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.ToDoAppDemo.repository;
+
+import com.example.ToDoAppDemo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
