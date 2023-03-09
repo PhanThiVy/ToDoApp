27596e8b92d5da0e94e199c3696507247494ec6d
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-09 10:16:33 +0700

    create taskListRequestDto in requestDto

---
+++ src/main/java/com/example/ToDoAppDemo/dto/requestDto/TaskListRequestDto.java
@@ -0,0 +1,19 @@
+package com.example.ToDoAppDemo.dto.requestDto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+public class TaskListRequestDto {
+    @NotEmpty
+    @Size(min = 1,max = 20)
+    private String taskListName;
+    @NotEmpty
+    private String userId;
+}
