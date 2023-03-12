1f623e8d50f1e9a71babe176d767255fbee7f48d
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-12 13:07:52 +0700

    declare 2 method addTask() and taskNameIsExistForAdd() in TaskService

--- src/main/java/com/example/ToDoAppDemo/service/iService/TaskService.java
+++ src/main/java/com/example/ToDoAppDemo/service/iService/TaskService.java
@@ -1,4 +1,9 @@
 package com.example.ToDoAppDemo.service.iService;
 
+import com.example.ToDoAppDemo.dto.requestDto.TaskRequestDto;
+import com.example.ToDoAppDemo.dto.responseDto.TaskResponseDto;
+
 public interface TaskService {
+    public TaskResponseDto addTask(String taskListId,TaskRequestDto taskRequestDto);
+    public void taskNameIsExistForAdd(String taskName,Long taskListId);
 }
