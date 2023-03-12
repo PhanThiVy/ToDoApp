e1d77243a461163ef7c77a727684e8df89504e14
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-12 14:09:04 +0700

    declare method getTask() and getTaskById in TaskService

--- src/main/java/com/example/ToDoAppDemo/service/iService/TaskService.java
+++ src/main/java/com/example/ToDoAppDemo/service/iService/TaskService.java
@@ -3,11 +3,11 @@ package com.example.ToDoAppDemo.service.iService;
 import com.example.ToDoAppDemo.dto.requestDto.TaskRequestDto;
 import com.example.ToDoAppDemo.dto.responseDto.TaskResponseDto;
 import com.example.ToDoAppDemo.model.Task;
-import com.example.ToDoAppDemo.model.TaskList;
 
 public interface TaskService {
     public TaskResponseDto addTask(String taskListId,TaskRequestDto taskRequestDto);
     public void taskNameIsExistForAdd(String taskName,Long taskListId);
 
     public Task getTask(String taskId);
+    public TaskResponseDto getTaskById(String taskId);
 }
