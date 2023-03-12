992c0f89d2bc5c1ff1f2f74381bd446bd355f326
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-12 13:07:03 +0700

    create method taskNameIsExistForAdd in TasKRepository.java

--- src/main/java/com/example/ToDoAppDemo/repository/TasKRepository.java
+++ src/main/java/com/example/ToDoAppDemo/repository/TasKRepository.java
@@ -1,11 +1,17 @@
 package com.example.ToDoAppDemo.repository;
 
 import com.example.ToDoAppDemo.model.Task;
+import com.example.ToDoAppDemo.model.TaskList;
 import com.example.ToDoAppDemo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface TasKRepository extends JpaRepository<Task,Long> {
-
+    @Query(value = "SELECT * FROM tasK t WHERE t.task_name = :taskName  and t.task_list_id= :taskListId", nativeQuery = true)
+    Optional<Task> taskNameIsExistForAdd(@Param("taskName") String taskName, @Param("taskListId") Long taskListId);
 }
