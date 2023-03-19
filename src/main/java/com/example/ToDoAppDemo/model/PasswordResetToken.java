bbbd73d8ee349ae9304f8b0478081311ecb1950e
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-19 20:45:03 +0700

    create PasswordResetToken.java in model

---
+++ src/main/java/com/example/ToDoAppDemo/model/PasswordResetToken.java
@@ -0,0 +1,29 @@
+package com.example.ToDoAppDemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "password_reset_token")
+public class PasswordResetToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String token;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    private LocalDateTime expiryDate;
+}
