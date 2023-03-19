cf22bf36df2dca0c1036b1bb046988504664d145
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-19 20:43:03 +0700

    create CurrentPasswordNotMatch

---
+++ src/main/java/com/example/ToDoAppDemo/exception/passwordException/CurrentPasswordNotMatch.java
@@ -0,0 +1,19 @@
+package com.example.ToDoAppDemo.exception.passwordException;
+
+import com.example.ToDoAppDemo.exception.ErrorDetail;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class CurrentPasswordNotMatch extends RuntimeException{
+    private final ErrorDetail errorDetail;
+
+    public CurrentPasswordNotMatch(int errorCode, String message) {
+        super();
+        this.errorDetail = new ErrorDetail().builder()
+                .errorCode(errorCode)
+                .message(message)
+                .build();
+    }
+}
