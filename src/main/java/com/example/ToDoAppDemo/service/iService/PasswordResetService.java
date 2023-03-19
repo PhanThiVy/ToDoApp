77f911b73721c79f5f02967a0ef2a3cb3cd73c93
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-19 20:46:06 +0700

    create PasswordResetService.java and declare method findByToken

---
+++ src/main/java/com/example/ToDoAppDemo/service/iService/PasswordResetService.java
@@ -0,0 +1,9 @@
+package com.example.ToDoAppDemo.service.iService;
+
+import com.example.ToDoAppDemo.model.PasswordResetToken;
+
+public interface PasswordResetService {
+    public PasswordResetToken generateToken(String email);
+    public void resetPassword(String token, String newPassword);
+    public void changePassword(String currentPassword, String newPassword);
+}
