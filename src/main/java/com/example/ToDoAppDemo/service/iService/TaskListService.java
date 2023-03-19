a612aa4430551c89fe0dbaa9aa1bb49b94cc5317
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-19 13:58:35 +0700

    declare method getAllTaskList in TaskListService.java

--- src/main/java/com/example/ToDoAppDemo/service/iService/TaskListService.java
+++ src/main/java/com/example/ToDoAppDemo/service/iService/TaskListService.java
@@ -3,6 +3,7 @@ package com.example.ToDoAppDemo.service.iService;
 import com.example.ToDoAppDemo.dto.requestDto.TaskListRequestDto;
 import com.example.ToDoAppDemo.dto.responseDto.TaskListResponseDto;
 import com.example.ToDoAppDemo.model.TaskList;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 public interface TaskListService {
@@ -10,6 +11,7 @@ public interface TaskListService {
     public void taskListNameIsExistForAdd(String taskListName,Long userId);
     public TaskList getTaskList(Long taskListById);
     public TaskListResponseDto getTaskListById(Long taskListById);
+    public Page<TaskListResponseDto> getAllTaskList(int pageNumber, Long userId);
     public TaskListResponseDto editTaskList(Long taskListId, TaskListRequestDto taskListRequestDto);
     public void taskListNameIsExistForEdit(String taskListName,Long taskListId,Long userId);
 }
