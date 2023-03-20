b0c29de5061f213c57280ac5c586f7224f2ecd00
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-20 13:31:44 +0700

    create api '/admin/blockUser' in AdminController.java

--- src/main/java/com/example/ToDoAppDemo/controller/AdminController.java
+++ src/main/java/com/example/ToDoAppDemo/controller/AdminController.java
@@ -1,12 +1,14 @@
 package com.example.ToDoAppDemo.controller;
 
 import com.example.ToDoAppDemo.dto.responseDto.UserResponseDto;
-import com.example.ToDoAppDemo.service.iService.UserService;
+import com.example.ToDoAppDemo.service.iService.AdminService;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,11 +16,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminController {
-    private final UserService userService;
+    private final AdminService adminService;
     @GetMapping("/getUserById")
     public ResponseEntity<UserResponseDto> getUserById(@RequestParam Long userId){
-        UserResponseDto userResponseDto = userService.getUserById(userId);
+        UserResponseDto userResponseDto = adminService.getUserById(userId);
         return new ResponseEntity<>(userResponseDto, HttpStatus.OK);
+    }
+
+    @PutMapping("/blockUser")
+    public ResponseEntity<UserResponseDto> blockUser(@RequestParam Long userId){
+        UserResponseDto userResponseDto = adminService.blockUser(userId);
+        return new ResponseEntity<>(userResponseDto,HttpStatus.OK
+        );
     }
 
 }
